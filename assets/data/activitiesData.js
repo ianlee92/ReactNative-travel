7c5da248d40fc2b0429a03e64dbe8959576b194a
ianlee92 <ethian9227@gmail.com>
2021-06-28 23:22:03 +0900

    data

--- assets/data/activitiesData.js
+++ assets/data/activitiesData.js
@@ -1,32 +1,32 @@
 const activitiesData = [
   {
     id: 'activities-1',
-    title: 'KAYAK',
+    title: '카약',
     image: require('../images/kayak-icon.png'),
   },
   {
     id: 'activites-2',
-    title: 'HIKE',
+    title: '하이킹',
     image: require('../images/mountain.png'),
   },
   {
     id: 'activites-3',
-    title: 'SPA',
+    title: '스파',
     image: require('../images/spa.png'),
   },
   {
     id: 'activites-6',
-    title: 'BIKE',
+    title: '자전거',
     image: require('../images/bike.png'),
   },
   {
     id: 'activites-5',
-    title: 'TENNIS',
+    title: '테니스',
     image: require('../images/tennis.png'),
   },
   {
     id: 'activites-4',
-    title: 'WATER SKI',
+    title: '수상스키',
     image: require('../images/water-ski.png'),
   },
 ];
---
+++ assets/data/learnMoreData.js
@@ -0,0 +1,24 @@
+const learnMoreData = [
+  {
+    id: 'learnMore-1',
+    title: 'Tour of the NY',
+    image: require('../images/newyork.png'),
+  },
+  {
+    id: 'learnMore-2',
+    title: 'Looking for Toronto',
+    image: require('../images/toronto.png'),
+  },
+  {
+    id: 'learnMore-3',
+    title: 'Surfing in Hawaii',
+    image: require('../images/hawaii.png'),
+  },
+  {
+    id: 'learnMore-4',
+    title: 'Enjoy Okinawa',
+    image: require('../images/okinawa.png'),
+  },
+];
+
+export default learnMoreData;
