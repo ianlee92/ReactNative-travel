a5b8dbe875fc788b35502d2007a7b56e631a2ede
ianlee92 <ethian9227@gmail.com>
2021-06-29 22:31:25 +0900

    top section title

--- components/Details.js
+++ components/Details.js
@@ -11,6 +11,7 @@ import colors from '../assets/colors/colors';
 import Entypo from 'react-native-vector-icons/Entypo';
 
 const height = Dimensions.get('window').height;
+
 const Details = ({route, navigation}) => {
   const {item} = route.params;
   return (
@@ -21,6 +22,13 @@ const Details = ({route, navigation}) => {
           onpress={() => navigation.goBack()}>
           <Entypo name="chevron-left" size={32} color={colors.white} />
         </TouchableOpacity>
+        <View style={styles.titlesWrapper}>
+          <Text style={styles.itemTitle}>{item.title}</Text>
+          <View style={styles.locationWrapper}>
+            <Entypo name="location-pin" size={24} color={colors.white} />
+            <Text style={styles.locationText}>{item.location}</Text>
+          </View>
+        </View>
       </ImageBackground>
       <View style={styles.descriptionWrapper} />
     </View>
@@ -34,6 +42,7 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     height: height * 0.6,
+    justifyContent: 'space-between',
   },
   descriptionWrapper: {
     flex: 1,
@@ -44,6 +53,25 @@ const styles = StyleSheet.create({
   backIcon: {
     marginLeft: 20,
     marginTop: 60,
+  },
+  titlesWrapper: {
+    marginHorizontal: 20,
+    marginBottom: 40,
+  },
+  itemTitle: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 32,
+    color: colors.white,
+  },
+  locationWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  locationText: {
+    fontFamily: 'NotoSansKR-Bold',
+    fontSize: 16,
+    color: colors.white,
   },
 });
 
