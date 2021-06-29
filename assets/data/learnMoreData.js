c9be457621c39010114bdc6c195dba744841b0e6
ianlee92 <ethian9227@gmail.com>
2021-06-29 22:25:01 +0900

    detail item, layout, backbtn

--- assets/data/learnMoreData.js
+++ assets/data/learnMoreData.js
@@ -1,22 +1,22 @@
 const learnMoreData = [
   {
     id: 'learnMore-1',
-    title: 'Tour of the NY',
+    title: '뉴욕 거리 누비기',
     image: require('../images/newyork.jpeg'),
   },
   {
     id: 'learnMore-2',
-    title: 'Looking for Toronto',
+    title: '토론토 관광명소들',
     image: require('../images/toronto.jpeg'),
   },
   {
     id: 'learnMore-3',
-    title: 'Surfing in Hawaii',
+    title: '하와이에서 서핑을',
     image: require('../images/hawaii.jpeg'),
   },
   {
     id: 'learnMore-4',
-    title: 'Enjoy Okinawa',
+    title: '오키나와 제대로 즐기기',
     image: require('../images/okinawa.jpeg'),
   },
 ];
--- components/Details.js
+++ components/Details.js
@@ -1,12 +1,50 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import colors from '../assets/colors/colors';
+import Entypo from 'react-native-vector-icons/Entypo';
 
-const Details = () => {
+const height = Dimensions.get('window').height;
+const Details = ({route, navigation}) => {
+  const {item} = route.params;
   return (
-    <View>
-      <Text>Details</Text>
+    <View style={styles.container}>
+      <ImageBackground source={item.imageBig} style={styles.backgroundImage}>
+        <TouchableOpacity
+          style={styles.backIcon}
+          onpress={() => navigation.goBack()}>
+          <Entypo name="chevron-left" size={32} color={colors.white} />
+        </TouchableOpacity>
+      </ImageBackground>
+      <View style={styles.descriptionWrapper} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  backgroundImage: {
+    height: height * 0.6,
+  },
+  descriptionWrapper: {
+    flex: 1,
+    backgroundColor: colors.white,
+    marginTop: -20,
+    borderRadius: 25,
+  },
+  backIcon: {
+    marginLeft: 20,
+    marginTop: 60,
+  },
+});
 
 export default Details;
